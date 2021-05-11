cd0aa7624e7632466c38d6fcba91da60e6e7c3bc
Carlos04 <cvargaslopez769@gmail.com>
2021-05-11 16:37:58 -0600

    working section user

--- proyecto/User-Profile/main_user.js
+++ proyecto/User-Profile/main_user.js
@@ -2,13 +2,17 @@
 const get_input = document.getElementById('get_reference')
 const preview_img = document.getElementById('preview_image')
 
-get_input.addEventListener("change", () => {
 
+get_input.addEventListener("change", () => {
+    
     let files_img = get_input.files;
     let user_profile = files_img[0]
-    
     let make_url = URL.createObjectURL(user_profile)
-    
+    localStorage.setItem('my_img', make_url)
     preview_img.src = make_url
+
+    if (location.reload) {
+        preview_img.src = localStorage.getItem('my_img')
+    }
 })
 
