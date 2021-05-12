62bf19e2b419829260dcd680bfb5370f6aa6f497
Carlos04 <cvargaslopez769@gmail.com>
2021-05-12 15:03:53 -0600

    user profile- img_user ok

--- proyecto/User-Profile/main_user.js
+++ proyecto/User-Profile/main_user.js
@@ -4,20 +4,23 @@ const preview_img = document.getElementById('preview_image');
 
 //function IIFE
 /*
-(() => {
-    let picture = localStorage.getItem('UserImg')
-    preview_img.src = picture
-})()
 */
+(() => {
+    let picture = JSON.parse(localStorage.getItem('UserImg'))
+    if (picture === null) {
+        preview_img.src = ""
+    } else {
+        preview_img.src = picture
+    }
+})()
 
 get_input.addEventListener("change", () => {
     
     let files_img = get_input.files[0];
     let make_url = URL.createObjectURL(files_img)
-    console.log(make_url);
     
     preview_img.src = make_url
     
-    // localStorage.setItem('UserImg',  JSON.stringify( make_url ))
+    localStorage.setItem('UserImg',  JSON.stringify( make_url ))
 })
 
