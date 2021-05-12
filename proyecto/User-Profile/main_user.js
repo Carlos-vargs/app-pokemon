5a22678a4841bc48a34a8c87bfe593aebe8deecc
Carlos04 <cvargaslopez769@gmail.com>
2021-05-12 11:52:57 -0600

    section user

--- proyecto/User-Profile/main_user.js
+++ proyecto/User-Profile/main_user.js
@@ -1,18 +1,23 @@
 // get the reference
-const get_input = document.getElementById('get_reference')
-const preview_img = document.getElementById('preview_image')
+const get_input = document.getElementById('get_reference');
+const preview_img = document.getElementById('preview_image');
 
+//function IIFE
+/*
+(() => {
+    let picture = localStorage.getItem('UserImg')
+    preview_img.src = picture
+})()
+*/
 
 get_input.addEventListener("change", () => {
     
-    let files_img = get_input.files;
-    let user_profile = files_img[0]
-    let make_url = URL.createObjectURL(user_profile)
-    localStorage.setItem('my_img', make_url)
+    let files_img = get_input.files[0];
+    let make_url = URL.createObjectURL(files_img)
+    console.log(make_url);
+    
     preview_img.src = make_url
-
-    if (location.reload) {
-        preview_img.src = localStorage.getItem('my_img')
-    }
+    
+    // localStorage.setItem('UserImg',  JSON.stringify( make_url ))
 })
 
