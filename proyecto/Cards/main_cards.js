ecf7f0cf06bf287435f874e4c620c980f676da3e
Carlos04 <cvargaslopez769@gmail.com>
2021-05-20 15:20:03 -0600

    add changes

--- proyecto/Cards/main_cards.js
+++ proyecto/Cards/main_cards.js
@@ -19,7 +19,7 @@ const modal_names = document.getElementById('pokemon-name')
 const poke_Damage = document.getElementById('pokeDamage')
 const poke_xp = document.getElementById('pokeXp')
 const poke_hab = document.getElementById('pokeHability')
-const color_target = document.getElementById('color-target')
+const btn_choose = document.getElementById('color-target')
 const poke_weight = document.getElementById('poke-weight')
 const poke_color = document.getElementById('poke-color')
 const poke_types = document.getElementById('pokeTypes')
@@ -35,6 +35,10 @@ const getPic = JSON.parse( localStorage.getItem('UserImg') )
 const button_select = document.getElementById('select-pokemon')
 const preload_cards = document.getElementById('preload') 
 const arr = []
+//btn select
+const btn_select = document.createElement('button')
+btn_select.classList.add("button_select-pokemon")
+btn_choose.appendChild(btn_select)
 
 function fetchDataPokemon(){
     fetch(URL)
@@ -56,29 +60,40 @@ function pokemonInformation(pokemon){
     })
 }
 
-function createTypes(types, ul){
+function createTypes(types, parent){
     types.forEach(function(e){
-    let typeLi = document.createElement('li')
+    let typeLi = document.createElement('p')
     typeLi.innerHTML = firstLetter(e.type.name)
     typeLi.classList.add('list_deleted')
-    ul.appendChild(typeLi)
+    parent.appendChild(typeLi)
     })
 }
 
-function createHability(hab, ul){
+function createHability(hab, parent){
     hab.forEach(function (a) {
-        let hability = document.createElement('li')
+        let hability = document.createElement('p')
         hability.innerHTML = firstLetter(a.ability.name)
-        ul.appendChild(hability)
+        hability.classList.add('list_deleted')
+        parent.appendChild(hability)
+    })
+}
+
+function poke_species(spc) {
+    let spc_inf = spc.url
+    fetch(spc_inf)
+    .then(res => res.json())
+    .then(function (b) {
+        let bio_ifn = b.flavor_text_entries[1].flavor_text
+        poke_bio.innerHTML = `Description: ${firstLetter(bio_ifn.toLowerCase())}`
+        /*b.color.name*/
     })
 }
 
 function renderPokemon(pokeData){
     let countHability = pokeData.abilities
     let countTypes = pokeData.types
-    let pokeBio = `https://pokeapi.co/api/v2/pokemon-species/${pokeData.id}/`
     let pokeImg = `https://pokeres.bastionbot.org/images/pokemon/${pokeData.id}.png`
-    
+
     let infCards = cards_generator(pokeData) 
     allPokemonContainer.appendChild(infCards);
 
@@ -96,32 +111,28 @@ function renderPokemon(pokeData){
         pokeContainerModal.style.display = 'flex';
         htmlScroll.style.overflow= 'hidden';
         poke_Img.src = pokeImg
-        poke_Img.alt = `Pokemon ${pokeData.name}` 
+        poke_Img.alt = pokeData.name 
         modal_names.innerHTML = firstLetter(pokeData.name)
         poke_xp.innerHTML = `Experience: ${pokeData.base_experience}`
         poke_weight.innerHTML = `Weight: ${pokeData.weight} kg`
+        btn_select.innerHTML = `Choose ${pokeData.name}`
+
 
         createTypes(countTypes, poke_types);
         createHability(countHability, poke_hab);
+        poke_species(pokeData.species)
 
-        button_select.addEventListener("click", () => {
-            arr.push({nombre: pokeData.name, img: `https://pokeres.astionbot.org/images/pokemon/${pokeData.id}.png`},)
+        btn_select.addEventListener("click", () => {
+
+            arr.push({nombre: pokeData.name, img: pokeImg},)
+            localStorage.setItem('pokemons', JSON.stringify(arr))
             console.log(arr);
-            localStorage.setItem('pokemon', JSON.stringify(arr))
-        });
-
-
-        fetch(pokeBio)
-        .then(res => isResOk(res))
-        .then(dataBio => {
-            let caseBio = dataBio.flavor_text_entries[1].flavor_text
-            poke_bio.innerHTML = `Description: ${firstLetter(caseBio.toLowerCase())}`
-            /*
-            color_target.style.backgroundColor = dataBio.color.name
-            if (dataBio.color.name === "yellow") {
-                color_target.style.backgroundColor = "#FFC000"
+            
+            if (arr.length === 5) {
+                arr[4].nombre = pokeData.name 
+                arr[4].img = pokeImg
+                arr.pop()
             }
-            */
         })
     })
 }
