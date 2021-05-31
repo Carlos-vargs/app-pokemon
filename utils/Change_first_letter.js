8384b8bb4d4bb945bb5f299687c3d0ceb86323a7
Carlos04 <cvargaslopez769@gmail.com>
2021-05-31 10:52:12 -0600

    add new changes

--- utils/Change_first_letter.js
+++
@@ -1,4 +0,0 @@
-//change the first letter to Uppercase
-function firstLetter(pokenam) {
-    return pokenam.charAt(0).toUpperCase() + pokenam.slice(1);
-}
--- utils/Fetch_evolution.js
+++
@@ -1,8 +0,0 @@
-const fetchDataEvolution = obj => {
-    fetch(obj)
-        .then(res => res.json())
-        .then( evo => {
-            let img = generate_img(evo)
-            mtm_evolution.appendChild(img)
-        })
-}
