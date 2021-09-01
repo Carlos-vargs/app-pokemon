e6a1e57b74bb0d157eb66c03ab808324f8e80f2c
Carlos-vargs <cvargaslopez769@gmail.com>
2021-09-01 11:34:43 -0600

    add

--- src/utils/Cards_Generator.js
+++ src/utils/Cards_Generator.js
@@ -5,7 +5,7 @@ const cards_generator = obj => {
     
      POKECARD.innerHTML = `
      <h3 class="conten">${firstLetter(obj.name)}</h3>
-     <img src="https://pokeres.bastionbot.org/images/pokemon/${obj.id}.png" alt="pokemon ${firstLetter(obj.name)}" class="img-target">
+     <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${obj.id}.png" alt="pokemon ${firstLetter(obj.name)}" class="img-target">
      `
     return POKECARD
 }
